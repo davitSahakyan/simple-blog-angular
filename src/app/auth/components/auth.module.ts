67f038f8ea1b4b6701b39a3b65775d98f6f60320
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-10-28 13:40:12 +0400

    Add form group contol

--- src/app/auth/components/auth.module.ts
+++ src/app/auth/components/auth.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import {NgModule} from '@angular/core'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations : [LoginComponent , RegisterComponent],
-  imports : [CommonModule],
+  imports : [CommonModule , ReactiveFormsModule , FormsModule],
   exports : [LoginComponent , RegisterComponent],
 })
 
--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,9 +1,24 @@
-import {Component} from '@angular/core'
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
-  selector : "app-login",
-  templateUrl : "./login.component.html",
-  styleUrls : ["./login.component.css"]
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
 })
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-export class LoginComponent{}
+  constructor() {}
+
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      login: new FormControl(''),
+      password: new FormControl(''),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.loginForm);
+  }
+}
