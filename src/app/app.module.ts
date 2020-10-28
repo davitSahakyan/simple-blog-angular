a8c5333f510f3a08a4dd0eb64bc9e2d57110e567
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-10-28 13:01:58 +0400

    Add auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/components/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
