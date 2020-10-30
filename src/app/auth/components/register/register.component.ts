6324cd5b3f699e6d362015e2e66a36afc15061ad
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-10-30 13:05:14 +0400

    Add form control part

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,9 +1,31 @@
-import {Component} from '@angular/core'
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
-  selector : "app-register",
-  templateUrl : "./register.component.html",
-  styleUrls : ["./register.component.css"]
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
 })
+export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
 
-export class RegisterComponent{}
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.registerForm = new FormGroup({
+      login: new FormControl(''),
+      name: new FormControl(''),
+      surname: new FormControl(''),
+      password: new FormControl(''),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.registerForm.value);
+  }
+
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
+}
